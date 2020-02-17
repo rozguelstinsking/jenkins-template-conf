2eedf26b67a003210850a820da08bfe1f87c712d
Luis de la peña <luis.delapena@hotmail.es>
2020-02-17 15:37:06 -0500

    Test for environment config

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -3,8 +3,8 @@ void call(){
     print "gradle: build() without args"
     print config.message
     print config.version	
-    for(int i=0, i < config.version, i++){
-        println config.message
-    }
+    for(int i=0;i < config.version;i++){
+	print config.message + i
+	}
 }
 
