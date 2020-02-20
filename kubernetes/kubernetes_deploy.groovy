f5c72485e51cbaeb01f88c2decd3451f50a8735d
Luis de la peña <luis.delapena@hotmail.es>
2020-02-20 12:55:17 -0500

    calling build job into freestyle project

--- kubernetes/kubernetes_deploy.groovy
+++ kubernetes/kubernetes_deploy.groovy
@@ -5,7 +5,7 @@ void call(){
 	stages:{
 
 		stage("deploy"){
-			sh '''puppet agent --test --certname jenkins.pem '''
+			//sh '''puppet agent --test --certname jenkins.pem '''
 			build 'build'
 			}
 		}
