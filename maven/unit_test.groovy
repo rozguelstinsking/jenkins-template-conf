5233c8914062eae3b3d63b69778e0e6b64b848b1
Luis de la peña <luis.delapena@hotmail.es>
2020-03-16 22:18:32 -0400

    looking for  env values

--- maven/unit_test.groovy
+++ maven/unit_test.groovy
@@ -1,7 +1,9 @@
 void call(){
     println "maven: change into library made by rozg"
 
-    print "environment: ${env.JOB_NAME}"
+    print "environment jobname: ${env.JOB_NAME}"
+
+    print "environment actual: ${env.dev.long_name}"
 
     //node("master"){
         sh("mvn test")
